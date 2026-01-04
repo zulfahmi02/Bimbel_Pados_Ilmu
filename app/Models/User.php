6041d3da3ab0cc346d5985a56e98df6484e4bf5c
zulfahmi02 <zulfahmihidayatulloh@gmail.com>
2026-01-05 00:28:16 +0700

    feat: Implement FilamentUser interface and add `canAccessPanel` method to the User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * The attributes that are mass assignable.
